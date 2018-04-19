dee01a18735d08171d69c8f927b0945b899d5df0
Lachlan MacKenzie <lachlanrm@gmail.com>
2018-04-18 22:04:09 -0700

    stage-4 --Create and return collection

--- index.js
+++ index.js
@@ -2,6 +2,22 @@
 
 const { graphql, buildSchema } = require('graphql')
 
+const videoA = {
+  id: 'A',
+  title: 'End of the World',
+  duration: 128,
+  watched: false
+}
+
+const videoB = {
+  id: 'B',
+  title: 'Begining of the End',
+  duration: 103,
+  watched: true
+};
+
+const videos = [videoA, videoB]
+
 const schema = buildSchema(`
   type Video {
     id: ID,
@@ -12,6 +28,7 @@ const schema = buildSchema(`
 
   type Query {
     video: Video
+    videos: [Video]
   }
 
   type Schema {
@@ -26,11 +43,12 @@ const resolvers = {
     duration: 6969,
     watched: true,
   }),
+  videos: () => videos
 };
 
 const query = `
   query myFirstQuery {
-    video{
+    videos {
       id,
       title,
       duration,
