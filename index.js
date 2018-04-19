b238fe53958b847974752c3679042838ca0f886b
Lachlan MacKenzie <lachlanrm@gmail.com>
2018-04-19 13:06:28 -0700

    stage-7 -- Use arguments in GQL query -- Extract video + methods to data layer

--- index.js
+++ index.js
@@ -10,46 +10,34 @@ const {
   GraphQLInt,
   GraphQLID,
 } = require('graphql');
+const { getVideoById } = require('./src/data/index')
 
 const PORT = process.env.PORT || 3000;
 const server = express();
 
-
-const videoA = {
-  id: 'A',
-  title: 'End of the World',
-  duration: 128,
-  watched: false
-}
-
-const videoB = {
-  id: 'B',
-  title: 'Begining of the End',
-  duration: 103,
-  watched: true
-};
-
-const videos = [videoA, videoB]
-
 const videoType = new GraphQLObjectType({
-  name: "Video",
-  description: "A video on some cool service",
+  name: 'Video',
+  description: 'A video on some cool service',
   fields: {
     id: {
       type: GraphQLID,
-      description: "The video ID"
+      description: 'The video ID'
     },
     title: {
       type: GraphQLString,
-      description: "The title of the Video"
+      description: 'The title of the Video'
     },
     duration: {
       type: GraphQLInt,
-      description: "Length of the video"
+      description: 'Length of the video'
     },
     watched: {
       type: GraphQLBoolean,
-      description: "Video has been viewed"
+      description: 'Video has been viewed'
+    },
+    name: {
+      type: GraphQLString,
+      description: 'Short name of video'
     }
   }
 });
@@ -60,62 +48,26 @@ const queryType = new GraphQLObjectType({
   fields: {
     video: {
       type: videoType,
-      resolve: () => new Promise((resolve) => {
-        resolve({
-          id: 'a',
-          title: 'GraphQL Magic',
-          duration: 134,
-          watch: false,
-        })
-      })
+      args: {
+        id: {
+          type: GraphQLID,
+          description: "ID of the video"
+        },
+        name: {
+          type: GraphQLString,
+          description: 'Name of required video'
+        }
+      },
+      resolve: (_, args ) => {
+        return getVideoById(args.id)
+      }
     }
   }
 })
 
 const schema = new GraphQLSchema({
   query: queryType,
-  // mutation,
-  // subscription,
 })
-
-// const schema = buildSchema(`
-//   type Video {
-//     id: ID,
-//     title: String,
-//     duration: Int,
-//     watched: Boolean
-//   }
-
-//   type Query {
-//     video: Video
-//     videos: [Video]
-//   }
-
-//   type Schema {
-//     query: Query
-//   }
-// `);
-
-// const resolvers = {
-//   video: () => ({
-//     id: '1',
-//     title: 'Poop',
-//     duration: 6969,
-//     watched: true,
-//   }),
-//   videos: () => videos
-// };
-
-// const query = `
-//   query myFirstQuery {
-//     videos {
-//       id,
-//       title,
-//       duration,
-//       watched
-//     }
-//   }
-// `;
 
 server.use('/graphql', graphqlHTTP({
   schema,
---
+++ src/data/index.js
@@ -0,0 +1,26 @@
+const videoA = {
+  id: 'A',
+  title: 'End of the World',
+  duration: 128,
+  watched: false,
+  name: 'EOTW'
+};
+
+const videoB = {
+  id: 'B',
+  title: 'Begining of the End',
+  duration: 103,
+  watched: true,
+  name: 'BOTE'
+};
+
+const videos = [videoA, videoB];
+
+const getVideoById = (id) => new Promise( (resolve) => { 
+  const [video] = videos.filter( (video) => {
+    return video.id === id;
+  });
+  resolve(video)
+})
+
+exports.getVideoById = getVideoById;
