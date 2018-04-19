2a30f741b67e4c1cf619501fece73278c86928f5
Lachlan MacKenzie <lachlanrm@gmail.com>
2018-04-18 21:49:42 -0700

    stage-3 --Update Query

--- index.js
+++ index.js
@@ -3,11 +3,15 @@
 const { graphql, buildSchema } = require('graphql')
 
 const schema = buildSchema(`
-  type Query {
+  type Video {
     id: ID,
     title: String,
     duration: Int,
     watched: Boolean
+  }
+
+  type Query {
+    video: Video
   }
 
   type Schema {
@@ -16,18 +20,22 @@ const schema = buildSchema(`
 `);
 
 const resolvers = {
-  id: () => '1',
-  title: () => 'barf',
-  duration: () => 69,
-  watched: () => true
+  video: () => ({
+    id: '1',
+    title: 'Poop',
+    duration: 6969,
+    watched: true,
+  }),
 };
 
 const query = `
   query myFirstQuery {
-    id,
-    title,
-    duration,
-    watched
+    video{
+      id,
+      title,
+      duration,
+      watched
+    }
   }
 `;
 
