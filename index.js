e60463f1705bc24bed5255790a99c7987a51eafa
Lachlan MacKenzie <lachlanrm@gmail.com>
2018-04-19 11:19:29 -0700

    stage-5 -- Add express and graphQL middleware -- Use gqlHTTP

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { graphql, buildSchema } = require('graphql');
 
-const { graphql, buildSchema } = require('graphql')
+const PORT = process.env.PORT || 3000;
+const server = express();
+
 
 const videoA = {
   id: 'A',
@@ -57,6 +62,12 @@ const query = `
   }
 `;
 
-graphql(schema, query, resolvers)
-  .then(result => console.log(result))
-  .catch(error => console.log(error));
+server.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+  rootValue: resolvers,
+}));
+
+server.listen( PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
